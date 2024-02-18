e6bacb36b70b0b3edfd1235310e6f61b556d37ce
Akhilesh Singh <akhilesh.singh@haud.com>
2024-02-18 18:15:16 +0530

    Changed some file names.

--- src/main/java/com/aone/poc/dbtocsvfile/DbToCsvfileApplication.java
+++ src/main/java/com/aone/poc/dbtocsvfile/DbToCsvfileApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbtcsvfileApplication {
+public class DbToCsvFileApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DbtocsvfileApplication.class, args);
+		SpringApplication.run(DbToCsvFileApplication.class, args);
 	}
 
 }
